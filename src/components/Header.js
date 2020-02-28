ac333afe5de2ab4efa14b93944468b97bff01d5c
Zahid Khawaja <zahidkhawaja@icloud.com>
2020-02-28 12:11:16 -0500

    Design tweaks

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,9 +7,9 @@ export default function Header() {
   return (
     <header className="ui centered">
       <h1 className="ui center">Rick &amp; Morty Fan Page</h1>
-      <nav>
-      <Link to ="/">Home</Link>
-      <Link to = "/characters">Characters</Link>
+      <nav className = "navigation">
+      <Link to ="/"><h2>Home</h2></Link>
+      <Link to = "/characters"><h2>Characters</h2></Link>
       </nav>
 
       <Switch>

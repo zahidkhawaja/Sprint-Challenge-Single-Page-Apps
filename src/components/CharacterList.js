1d40af5cd8e33650972f1d58482f276bd7d68e9e
Zahid Khawaja <zahidkhawaja@icloud.com>
2020-02-28 11:55:31 -0500

    Add search functionality, delete unnecessary components

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -8,24 +8,42 @@ export default function CharacterList() {
   useEffect(() => {
     axios.get("https://rickandmortyapi.com/api/character/")
       .then(response => {
-        console.log("FETCHED DATA");
+        console.log("Fetched data from API");
         const characters = response.data.results.filter(character => character.name.toLowerCase().includes(query.toLowerCase())
         );
         setData(characters);
       });
   }, [query]);
 
+  const handleInputChange = event => {
+    setQuery(event.target.value);
+  };
+
   return (
-    <div>
+
+    // Div for entire page
+  <div className = "characterspage">
+
+    {/* Search form section */}
+    <section className="search-form">
+     <form className = "search">
+       <input type = "text" onChange = {handleInputChange} value = {query} name = "name" tabIndex = "0" placeholder = "Search by name" autoComplete = "off" />
+     </form>
+    </section>
+
+    {/* Div for characters rendered from API */}
+    <div className = "renderedcharacters">
       {data.map(data => {
         return (
           <section className="character-list">
+          <img className = "image" src = {data.image}></img>
           <h3>Name: {data.name}</h3>
           <h3>Status: {data.status}</h3>
           <h3>Species: {data.species}</h3>
           </section>
         )
       })}
+      </div>
       </div>
   );
 }

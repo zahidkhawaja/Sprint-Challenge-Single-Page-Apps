2243d17749cab224ac7b94c4b17a34fa1c63addd
Zahid Khawaja <zahidkhawaja@icloud.com>
2020-02-28 11:34:51 -0500

    Add routes to pages, render characters from API

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,31 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function CharacterList() {
-  // TODO: Add useState to track data from useEffect
+  const [data, setData] = useState([]);
+  const [query, setQuery] = useState("");
 
   useEffect(() => {
-    // TODO: Add API Request here - must run in `useEffect`
-    //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
-  }, []);
+    axios.get("https://rickandmortyapi.com/api/character/")
+      .then(response => {
+        console.log("FETCHED DATA");
+        const characters = response.data.results.filter(character => character.name.toLowerCase().includes(query.toLowerCase())
+        );
+        setData(characters);
+      });
+  }, [query]);
 
   return (
-    <section className="character-list">
-      <h2>TODO: `array.map()` over your state here!</h2>
-    </section>
+    <div>
+      {data.map(data => {
+        return (
+          <section className="character-list">
+          <h3>Name: {data.name}</h3>
+          <h3>Status: {data.status}</h3>
+          <h3>Species: {data.species}</h3>
+          </section>
+        )
+      })}
+      </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,27 @@
 import React from "react";
+import { Link, Route, Switch } from "react-router-dom";
+import WelcomePage from "./WelcomePage";
+import CharacterList from "./CharacterList";
 
 export default function Header() {
   return (
     <header className="ui centered">
       <h1 className="ui center">Rick &amp; Morty Fan Page</h1>
+      <nav>
+      <Link to ="/">Home</Link>
+      <Link to = "/characters">Characters</Link>
+      </nav>
+
+      <Switch>
+      <Route path = "/characters">
+        <CharacterList/>
+      </Route>
+
+      <Route path = "/">
+        <WelcomePage/>
+      </Route>
+      </Switch>
+
     </header>
   );
 }
